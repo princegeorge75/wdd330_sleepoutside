e8cc7b5cce1a1083889f80acbb1c87495b3a554f
mserdarozden <mserdarozden@me.com>
2025-01-19 23:09:23 +0300

    running commit

--- src/js/product.js
+++ src/js/product.js
@@ -1,18 +1,13 @@
-import { setLocalStorage } from "./utils.mjs";
+import {setLocalStorage, getParams} from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
+const productId = getParams('product');
 
-function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
+//console.log(productId)
+//console.log(dataSource.findProductById(productId));
 
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+
+const product = new ProductDetails(productId, dataSource);
+product.init();
