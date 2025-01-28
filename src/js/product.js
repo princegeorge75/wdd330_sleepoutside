ab8b067d06a4de7639a52823cebb7f2f34c10f94
mserdarozden <43015687+mserdarozden@users.noreply.github.com>
2025-01-28 21:39:46 +0300

    Update product.js

--- src/js/product.js
+++ src/js/product.js
@@ -1,4 +1,4 @@
-import { setLocalStorage, getParams } from "./utils.mjs";
+import {getParams } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
