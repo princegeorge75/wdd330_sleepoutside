a69e00c35618b014dedfb306dc89f26c3f577b1a
mserdarozden <43015687+mserdarozden@users.noreply.github.com>
2025-02-02 18:47:55 +0300

    shoppingCart.mjs and partials added

---
+++ src/js/ShoppingCart.mjs
@@ -0,0 +1,37 @@
+import { getLocalStorage } from "./utils.mjs";
+import { renderListWithTemplate } from "./utils.mjs";
+
+
+function productCardTemplate(product) {
+    return `<li class="cart-card divider">
+  <a href="#" class="cart-card__image">
+    <img
+      src="${product.Image}"
+      alt="${product.Name}"
+    />
+  </a>
+  <a href="#">
+    <h2 class="card__name">${product.Name}</h2>
+  </a>
+  <p class="cart-card__color">${product.Colors[0].ColorName}</p>
+  <p class="cart-card__quantity">qty: 1</p>
+  <p class="cart-card__price">$${product.FinalPrice}</p>
+</li>`;
+}
+
+export default class ShoppingCart {
+    constructor(dataSource, listElement) {
+        this.dataSource = dataSource;
+        this.listElement = listElement;
+      }
+
+    init() {
+        // Fetch the product list
+        const cartItems = getLocalStorage(this.dataSource) || [];
+        this.renderCartContents(cartItems);
+      }
+
+      renderCartContents(list) {
+        renderListWithTemplate(productCardTemplate, this.listElement, list)
+      }
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,11 @@
 import ProductData from "./ProductData.mjs";
 import ProductListing from "./ProductList.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
 
 const dataSource = new ProductData("tents");
 
 const listElement = document.getElementById("product-list");
 const productList = new ProductListing("tents", dataSource, listElement);
 productList.init();
+
+loadHeaderFooter();
