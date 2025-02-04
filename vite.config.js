eb4d5d4d094b9132622e073cc85cd8d571828ddd
princegeorge75 <princegeorgexyx@gmail.com>
2025-02-04 08:46:23 +0300

    GN--Individual3

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import ProductListing from "./src/js/ProductList.mjs";
 
 export default defineConfig({
   root: "src/",
@@ -11,8 +12,8 @@ export default defineConfig({
         main: resolve(__dirname, "src/index.html"),
         cart: resolve(__dirname, "src/cart/index.html"),
         checkout: resolve(__dirname, "src/checkout/index.html"),
-        product: resolve(__dirname, "src/product_pages/index.html"
-        ),
+        product: resolve(__dirname, "src/product_pages/index.html"),
+        productListing: resolve(__dirname, "src/product-listing/index.html"),
       },
     },
   },
