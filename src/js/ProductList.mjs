a39e45ba4c5f21b20baf500a6c4ea0a963533469
princegeorge75 <princegeorgexyx@gmail.com>
2025-02-14 21:34:32 +0300

    step 4 -5

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,11 +1,32 @@
-export default class ProductListing {
-    constructor(products) {
-        this.products = products;
-    }
+import { renderListWithTemplate } from "./utils.mjs";
 
-    generateProductCards() {
-        if (!this.products || this.products.length === 0) {
-            return "<p>No products available.</p>";  //if no products are provided, return a message
-        }
-    }
+function productCardTemplate(product) {
+  return `<li class="product-card">
+    <a href="product_pages/index.html?product=${product.Id}">
+      <img src="${product.Image}" alt="Image of ${product.NameWithoutBrand}">
+      <h3 class="card__brand">${product.Brand.Name}</h3>
+      <h2 class="card__name">${product.NameWithoutBrand}</h2>
+      <p class="product-card__price">$${product.FinalPrice.toFixed(2)}</p>
+    </a>
+  </li>`;
+}
+
+export default class ProductListing {
+  constructor(category, dataSource, listElement) {
+    this.category = category;
+    this.dataSource = dataSource;
+    this.listElement = listElement;
+  }
+
+  async init() {
+    // Fetch the product list
+    const productList = await this.dataSource.getData();
+    this.renderList(productList);
+    // Render the product list into the DOM
+
+  }
+
+  renderList(list) {
+    renderListWithTemplate(productCardTemplate, this.listElement, list);
+  };
 }
