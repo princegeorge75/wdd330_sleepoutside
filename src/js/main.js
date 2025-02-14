1e6f58bf9dddbfe1ffcc75d5807b17245f681810
princegeorge75 <princegeorgexyx@gmail.com>
2025-02-14 21:39:31 +0300

    main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,8 @@
 import ProductData from "./ProductData.mjs";
+import ProductListing from "./ProductList.mjs";
 
 const dataSource = new ProductData("tents");
+
+const listElement = document.getElementById("product-list");
+const productList = new ProductListing("tents", dataSource, listElement);
+productList.init();
